69d95ba8f0f84be96eb4a6a50ec1d31496f31721
Vladimir Đorđević <vlada.dj@protonmail.com>
2025-07-09 12:48:16 +0200

    initial testing of gspread api

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+from gspread.utils import ValueInputOption
 import extract
 import fitz
+import gspread
+
+gc = gspread.oauth(credentials_filename="./credentials.json")
+try:
+    sheet = gc.open("the sheet")
+except gspread.exceptions.SpreadsheetNotFound:
+    sheet = gc.copy("1lBct8NGRyz0lV3qLV-MuE3olsJl_871OibU6ExkoMrE", "the sheet")
+
+qualityWorksheet = sheet.worksheet("Quality")
+priceWorksheet = sheet.worksheet("Price")
+
+qualityWorksheet.batch_update([
+    {
+        'range': 'E18',
+        'values': [['2']]
+    }
+], value_input_option=ValueInputOption.user_entered)
+
+text = "9"
+number = "21"
+priceWorksheet.batch_update([
+    {
+        'range': 'Q33:R33',
+        'values': [[text]]
+    },
+    {
+        'range': 'Q35:R35',
+        'values': [[number]]
+    }
+], value_input_option=ValueInputOption.user_entered)
 
 doc1name = "/home/vlada/Desktop/VL KO 2301.pdf"
 doc2name = "/home/vlada/Desktop/VL META 2302.pdf"
